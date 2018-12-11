ecfb72e5da3241fa5184957cab4163e0d1cc01c1
phankhoi654321 <phankhoi654321@gmail.com>
2018-12-11 08:13:40 +0700

    Add project files.

---
+++ AuthDemo01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AuthDemo01.MvcApplication" Language="C#" %>
---
+++ AuthDemo01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthDemo01.Startup))]
+namespace AuthDemo01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
